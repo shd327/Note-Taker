2ced044e80be3fe90c3ad3ba4eeffe03524c3e91
Hiro Protagonist <shd327@gmail.com>
2022-11-24 11:14:26 -0700

    rewrote the delete route and added commets

--- route/api.js
+++ route/api.js
@@ -8,6 +8,9 @@ const path = require("path");
 const uuid = require("../helpers/uuid");
 const router = require("express").Router();
 
+// get route
+// On a get request on /notes we create a dbpath const to store the db path
+// then we use the readFromFile helper function to read the json file and return the json
 router.get("/notes", (req, res) => {
     const dbPath = path.join(__dirname, "../db/db.json");
     readFromFile(dbPath)
@@ -18,7 +21,12 @@ router.get("/notes", (req, res) => {
             res.status(500).json(err);
         });
 });
-
+// post route
+// On a post request on /notes we create a dbpath const to store the db path
+// We deconstruct the red.body to two const title and text
+// We check if bothj title and text are not null and create a new note with an id using uuid
+// then we use the readAndAppend helper function which takes in the newNote and dbPath
+// lastly we send the response back or throw or error
 router.post("/notes", (req, res) => {
     const dbPath = path.join(__dirname, "../db/db.json");
     const { title, text } = req.body;
@@ -39,35 +47,30 @@ router.post("/notes", (req, res) => {
     }
 });
 
+// Delete route
+// On a delete request on /notes/:id we create a dbpath const to store the db path
+// We deconstruct the red.body to grab the id value
+// then we use the readAndAppend helper function which takes in the newNote and dbPath
+// then we use the readFromFile helper function which takes in the dbPath
+// We then take the json from the json file and parse it using JSON.pase and store it into parseNotes
+// Then we filter the parseNotes by exclude the id from the item delete and return it to a new array called notesArray
+// then we use the writeToFile helper function which overwrites the json file without the object which had the delete id
+// lastly we send the response back or throw or error
 router.delete("/notes/:id", (req, res) => {
     const dbPath = path.join(__dirname, "../db/db.json");
     const id = req.params.id;
-    console.log(id)
     readFromFile(dbPath)
-
 
         .then((notes) => {
             var parseNotes = JSON.parse(notes)
-            var notesArray = []
-            var newNotes = parseNotes.filter((note) => {
-                console.log(note)
-                console.log(note)
-
-                note.id !== id.toString();
-                if (note.id !== id) {
-                    notesArray.push(note)
-                }
-            });
-            console.log(notesArray)
+            const notesArray = parseNotes.filter((note) => note.id !== id);
             writeToFile(dbPath, notesArray);
         })
-        // .then((newNotes) => {
-        //     writeToFile(dbPath, newNotes);
-        // })
         .then(() => {
-            res.json({ ok: true })
+            res.json(`Item ${id} has been deleted 🗑️`)
+        }).catch((err) => {
+            res.status(500).json(err);
         })
-    // read file loop if id matches button than delete
 });
 
 module.exports = router;
--- route/html.js
+++ route/html.js
@@ -1,10 +1,11 @@
 const path = require('path')
 const router = require('express').Router()
 
+// Route / to /public/index.html
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
-
+// Route notes to /public/notes.html
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
